38f495ba824def4547825c85b1516a8d4ebeb8ab
Colin <hungrymamamia@gmail.com>
2023-06-11 19:25:43 -0700

    Updated to be able to open .csv files.

---
+++ data.py
@@ -0,0 +1,80 @@
+import csv
+# [auto_cone, teleop_cone, cycle_time, auto_cube, teleop_cube, auto_balance]
+#equipes = {"NRG - 948": [0, 0, 17, 13, 21, 12], "Sushi Squad - 7461": [6, 13, 15, 10, 7, 0], "Jack in the Bot - 2910": [4.5, 16, 13.5, 6, 15, 4.2], "Citrus Circuits - 1678": [7.286, 18.286, 10, 4.714, 11.857, 3.429]}
+
+def get_csv_data(filename):
+  fields = []
+  match_list = []
+  teams = {}
+  # Opens data and stores it as a list of lists
+  with open("dummydata.csv","r",encoding="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+    for row in csvreader:
+      match_list.append(row)
+  for match in match_list:
+    # Initialize variable
+    match_stats = []
+
+    # Calculates 5 of 6 stats represented in the hexagon
+    match_stats.append(int(match[10]) + int(match[11]) + int(match[12])) # Auto cone
+    match_stats.append(int(match[21]) + int(match[22]) + int(match[23])) # Teleop cone
+    match_stats.append(135 // (match_stats[1] + int(match[18]) + int(match[19]) + int(match[20]))) # Calculates cycle time by dividing 135 (seconds of teleop) by total pieces in teleop
+    match_stats.append(int(match[7]) + int(match[8]) + int(match[9])) # Auto cube
+    match_stats.append(int(match[18]) + int(match[19]) + int(match[20])) # Teleop cube
+
+    # Calculates auto scoring points (6th stat)
+    if match[13] == "1":
+      match_stats.append(12)
+    elif match[13] == "2":
+      match_stats.append(8)
+    else:
+      match_stats.append(0)
+
+    # Reformats team name
+    team_name = match[3].split(",")
+    team_name = team_name[3] + " - " + team_name[2]
+
+    # Records data, if multiple instances of same team, records list to be averaged later
+    try:
+      teams[team_name]
+      teams[team_name] = [teams[team_name], match_stats]
+    except:
+      teams[team_name] = match_stats
+
+  # Replaces stats of teams w/ multiple matches in the dataset with one set of average stats
+  for team in teams:
+    team_data = teams[team]
+    # Checks for a list
+    if type(team_data[0]) == type([]):
+
+      # Records total stats from all matches
+      total_stats = [0,0,0,0,0,0]
+      for datapoint in team_data:
+        for stat in range(6):
+          total_stats[stat] += datapoint[stat]
+      # Divides total stats by number of matches (therefore finding the average)
+      average_stats = [0,0,0,0,0,0]
+      for i in range(6):
+        average_stats[i] = total_stats[i] / len(team_data)
+      teams[team] = average_stats
+  return(teams)
+print(get_csv_data("dummydata"))
+def findMaxStat(statIndex,local_teams):
+  max = 0
+  min = 999
+  rv = True
+  for key in local_teams.keys():
+    value = local_teams.get(key)
+
+    if statIndex == 2:
+      rv = False
+      if value[statIndex] < min:
+        min = value[statIndex]
+    else:
+      if value[statIndex] > max:
+        max = value[statIndex]
+        
+  if rv:
+    return max
+  return min
---
+++ main.py
@@ -0,0 +1,100 @@
+import pygame, sys
+import data
+import math
+from pygame.locals import QUIT
+
+pygame.init()
+w = pygame.display.set_mode((400, 400))
+pygame.display.set_caption('Hexagonal Rating')
+
+# categories
+# Auto balance points
+# Cone auto points
+# Cone teleop points
+# Cube teleop points
+# Cube auto points
+# Cycle time
+# 
+
+  
+while True:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Draws circle around the hexagon
+    w.fill((255, 255, 255))
+    pygame.draw.circle(w, (211, 219, 232), (200, 200), 175)
+    
+    # Calculate the side length of the hexagon
+    hexagon_side = 350 / math.sqrt(3)
+    
+    # Calculate the points of the hexagon
+    hexagon_points = []
+    for i in range(6):
+        angle_deg = 60 * i
+        angle_rad = math.radians(angle_deg)
+        x = 200 + 175 * math.cos(angle_rad)
+        y = 200 + 175 * math.sin(angle_rad)
+        hexagon_points.append((x, y))
+    
+    # Draw the hexagon
+    pygame.draw.polygon(w, (169, 176, 171), hexagon_points)
+  
+    # Draws lines inside the hexagon that extend 175px from the center to each vertex
+    pygame.draw.line(w, (0, 0, 0), (25, 200), (375, 200))
+    pygame.draw.line(w, (0, 0, 0), (112.50000000000004, 351.55444566227675), (287.5, 48.44555433772325))
+    pygame.draw.line(w, (0, 0, 0), (287.5, 351.55444566227675), (112.49999999999993, 48.44555433772328))
+
+    teams = data.get_csv_data("dummydata")
+    auto_cone_max = data.findMaxStat(0,teams) # highest auto balance points
+    teleop_cone_max = data.findMaxStat(1,teams) # highest auto balance points
+    cycle_time_min = data.findMaxStat(2,teams) # highest auto balance points
+    auto_cube_max = data.findMaxStat(3,teams) # highest auto balance points
+    teleop_cube_max = data.findMaxStat(4,teams) # highest auto balance points
+    auto_balance_max = data.findMaxStat(5,teams) # highest auto balance points
+    #print(auto_cone_max,teleop_cone_max,cycle_time_min,auto_cube_max,teleop_cone_max,auto_balance_max)
+    team = "Bionic Black Hawks - 2834" # define this part
+    values = teams[team]
+    print(values[2])
+
+    # AUTO CONE
+    length = 175 #155.520905479
+    percentage = (float(values[0]) / auto_cone_max)
+    newLength = length * percentage
+    auto_cone_coord = ((200 - math.cos(1.0472) * newLength), (200 - math.sin(1.0472) * newLength))
+
+    # TELEOP CONE
+    percentage = (float(values[1]) / teleop_cone_max)
+    newLength = length * percentage
+    teleop_cone_coord = ((200 + math.cos(1.0472) * newLength), (200 - math.sin(1.0472) * newLength))
+
+    # CYCLE TIME
+    percentage = (cycle_time_min / float(values[2]))
+    newLength = length * percentage
+    cycle_time_coord = (200 + newLength, 200)
+
+    # AUTO CUBE
+    percentage = (float(values[3]) / auto_cube_max)
+    newLength = length * percentage
+    auto_cube_coord = ((200 + math.cos(1.0472) * newLength), (200 + math.sin(1.0472) * newLength))
+
+    # TELEOP CUBE
+    percentage = (float(values[4]) / teleop_cube_max)
+    newLength = length * percentage
+    teleop_cube_coord = ((200 - math.cos(1.0472) * newLength), (200 + math.sin(1.0472) * newLength))
+
+    # AUTO BALANCE
+    percentage = (float(values[5]) / auto_balance_max)
+    newLength = length * percentage
+    auto_balance_coord = (200 - newLength, 200)
+
+    coords = [auto_cone_coord, teleop_cone_coord, cycle_time_coord, auto_cube_coord, teleop_cube_coord, auto_balance_coord]
+
+    #for i in coords:
+      #pygame.draw.circle(w, (0, 0, 0), i, 10)
+  
+    pygame.draw.polygon(w, (0, 0, 0), coords)
+    pygame.display.update()
